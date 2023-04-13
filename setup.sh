c456815d799de318a4b7616d03a0c603034eb3f0
deepaerial <nazarii.oleksiuk@gmail.com>
2023-04-14 00:49:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,10 +25,33 @@ function print_info() {
 
 platform=$(uname)
 
+# Install Oh My ZSH
+# https://ohmyz.sh/#install
+print_info "Installing Oh-My-ZSH..."
+if command -v curl &> /dev/null; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+fi
+
 # Hush login on terminal start in Mac OS
 if [[ $platform == 'Darwin' ]]; then
     touch $HOME/.hushlogin
     print_success "Login prompt hushed..."
 fi
 
+# Creating symlinks
 ./makesymlinks.sh
+
+# Running brew bundle
+if [[ $platform == 'Darwin' ]]; then
+    if ! command -v brew &> /dev/null; then
+        print_info "Homebrew not found. Installing..."
+        # Installation instructions are here https://brew.sh/
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi 
+    print_info "Installing packages from Brewfile"
+    brew bundle --file="$HOME/Brewfile"
+fi
+
+print_success "Setup complete."
